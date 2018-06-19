48e4a8fc92d18f370439e792d8b0ed86124635f4
Mehdi Druon <mehdidruon@protonmail.com>
2018-06-19 20:19:01 +0200

    Add CountryCard component

---
+++ src/components/CountryCards.js
@@ -0,0 +1,38 @@
+// @flow
+
+import React from 'react';
+import Flag from 'react-flags';
+
+const divContainerStyle = {
+	border: 'gray',
+	borderRight: '2px',
+	display: 'flex',
+	alignItems: 'center',
+	borderRadius: '25px',
+};
+
+const spanStyle = {
+  display: 'block',
+	textColor: 'dark',
+	fontWeight: 'bold',
+};
+
+const CountryCard = props => {
+	const { cca2: code2 = '', region = null, name = {} } = props.country || {};
+
+	return(
+		<div className="col-sm-6 col-md-4 country-card">
+			<div style={divContainerStyle} className="country-card-container">
+				<div className="h-100 position-relative border-gray border-right px-2 bg-white rounded-left">
+					<Flag country={code2} format="png" pngSize={64} basePath="./img/flags" className="d-block h-100" />
+				</div>
+				<div className="px-3">
+					<span style={spanStyle} className="country-name d-block">{ name.common }</span>
+					<span className="country-region text-secondary text-uppercase">{ region }</span>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default CountryCard;
